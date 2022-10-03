f68367a1bc3f8ca1b5ba5565732962785aa9afbc
tatha0003@gmail.com <tbiswas3@agave3.agave.rc.asu.edu>
2022-10-03 11:49:10 -0700

    updated on 08.03.2022

--- config.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Tathagata Biswas <tbiswas3@asu.edu>'
-
-# TODO: @computron should be able to load from YAML -computron
-
-VASP_CMD = ">>vasp_cmd<<"
-SUMO_CMD = ">>sumo_cmd<<"
-WANNIER_CMD = ">>wannier_cmd<<"
-DB_FILE = ">>db_file<<"
-
